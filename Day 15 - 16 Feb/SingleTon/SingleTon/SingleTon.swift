4413545b05470449d261ac6ddfda701a139b2c84
Gurpreet Singh Bhachu <35268356+gurpreetbhachu@users.noreply.github.com>
2018-02-16 08:51:09 -0500

    Add files via upload

---
+++ Day 15 - 16 Feb/SingleTon/SingleTon/SingleTon.swift
@@ -0,0 +1,34 @@
+//
+//  SingleTon.swift
+//  SingleTon
+//
+//  Created by MacStudent on 2018-02-16.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class MySingleton
+{
+    private static var instant = MySingleton()
+    
+    private init()
+    {}
+    
+    static func getInstant() -> MySingleton
+    {
+        if instant != nil
+        {
+            return instant
+        }
+        else
+        {
+            instant = MySingleton()
+            return instant
+        }
+    }
+    
+    func getMyName() -> String{
+        return "GS"
+    }
+}
---
+++ Day 15 - 16 Feb/SingleTon/SingleTon/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SingleTon
+//
+//  Created by MacStudent on 2018-02-16.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+//print("Hello, World!")
+
+//'MySingleton' initializer is inaccessible due to 'private' protection level
+//var s1 = MySingleton()
+//print(MySingleton.instant.getMyName());
+
+print(MySingleton.getInstant().getMyName())
